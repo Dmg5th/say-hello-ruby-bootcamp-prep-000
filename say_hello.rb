3f42d9e92104b8a357a7f7365cf5b8ffcb40754d
Daniel Gould <dmgould310@gmail.com>
2020-07-06 13:59:20 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,9 @@
 # Build your say_hello method here
 
-
+def say_hello(name)
+puts "Hello Kent Beck"
+end
 
 def say_hello(name="Ruby Programmer!")
-  puts "Hello #{name}!"
+  puts ("Hello #{name}!")
 end 
-
-say_hello
-
